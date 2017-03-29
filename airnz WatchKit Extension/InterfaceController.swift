777443db076db655d7950071f97be4f4dd0f5410
Claudius Mbemba <mbembac@gmail.com>
2017-03-28 22:10:14 -0700

    audio player working

--- airnz WatchKit Extension/InterfaceController.swift
+++ airnz WatchKit Extension/InterfaceController.swift
@@ -9,6 +9,9 @@
 import WatchKit
 import Foundation
 
+let TEXT_ENDPOINT = "http://wavy.azurewebsites.net/speech/text";
+let AUDIO_ENDPOINT = "http://wavy.azurewebsites.net/speech/audio";
+
 class InterfaceController: WKInterfaceController {
     
     @IBOutlet var recordButton: WKInterfaceButton!
@@ -19,23 +22,18 @@ class InterfaceController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
-        self.playButton.setEnabled(false)
+        
+        //        self.playButton.setEnabled(false)
         let fileManager = FileManager.default
         
         let container =
             fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.airnz")
-        
-        let fileName = "audioFile.wav"
 
-//        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-//        saveUrl = NSURL.init(string: documentsPath.appending("/"+fileName));
+        let fileName = "audioFile.wav"
         
         saveUrl = container?.appendingPathComponent(fileName) as NSURL?;
-
-        //swift
-        NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(saveUrl)   \n\n\n==========================================\n");
         
+        NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(saveUrl)   \n\n\n==========================================\n");
     }
     
     @IBAction func recordAudio() {
@@ -46,70 +44,76 @@ class InterfaceController: WKInterfaceController {
             [WKAudioRecorderControllerOptionsMaximumDurationKey : duration,
              WKAudioRecorderControllerOptionsActionTitleKey: "Save"] as [String : Any]
         
-        presentAudioRecorderController(withOutputURL: saveUrl! as URL,
-                                                    preset: .narrowBandSpeech,
-                                                    options: recordOptions,
-                                                    completion: {
-                                                        saved, error in
-                                                        
-                                                        if let err = error {
-                                                            print(err.localizedDescription)
-                                                        }
-                                                        
-                                                        if saved {
-//                                                            self.playButton.setEnabled(true)
-                                                            self.postToApi()
-                                                        }
+        presentAudioRecorderController(
+            withOutputURL: saveUrl! as URL,
+            preset: .narrowBandSpeech,
+            options: recordOptions,
+            completion: {
+                saved, error in
+                
+                if let err = error {
+                    print(err.localizedDescription)
+                }
+                
+                if saved {
+                    //  self.playButton.setEnabled(true)
+                    self.postToApi(api: AUDIO_ENDPOINT)
+                }
         })
-                                        
+        
     }
     
     @IBAction func playAudio() {
+        playAudioWithMediaPlayer(url: saveUrl as! URL)
+        //        let fileURL = Bundle.main.url(forResource: "b001", withExtension: "wav")
+        //        playRecordedAudio(url: fileURL!)
         
+    }
+    
+    func playAudioWithMediaPlayer(url: URL) {
         let options = [WKMediaPlayerControllerOptionsAutoplayKey : "true"]
         
-        presentMediaPlayerController(with: saveUrl! as URL,
-                                     options: options,
-                                     completion: {
-                                        didPlayToEnd, endTime, error in
-                                                if let err = error {
-                                                    NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(err)   \n\n\n==========================================\n");
-                                                }
+        presentMediaPlayerController(
+            with: url,
+            options: options,
+            completion: { didPlayToEnd, endTime, error in
+                if let err = error {
+                    NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(err)   \n\n\n==========================================\n");
+                }
         })
     }
     
-    //Makes Http request
-    //Adapted from http://jamesonquave.com/blog/making-a-post-request-in-swift/
     @IBAction func postToSpeech(){
-        postToApi()
+        postToApi(api: AUDIO_ENDPOINT)
     }
     
     //Manually builds the headers needed
     //Adapted from https://stackoverflow.com/questions/4674982/uploading-audio-file-using-http-post-in-objective-c
     func prepareHttpBody(data: NSData) -> NSData {
-    
+        
         let boundary = "---------------------------14737809831466499882746641449"
-
+        
         let postData = NSMutableData.init()
         let header: String = String.init(format: "--%@\r\n", boundary)
         postData.append(header.data(using: String.Encoding.utf8)!)
         
-        //add your filename entry
         let contentDisposition = String.init(format: "Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n", "watch.wav", "watch.wav")
         
         postData.append(contentDisposition.data(using: .utf8)!)
         
         postData.append(data as Data)
-
+        
         let footer = String.init(format: "\r\n--%@\r\n", boundary)
         
         postData.append(footer.data(using: .utf8)!)
         return postData
     }
     
-    func postToApi(){
+    //Makes Http request
+    //Adapted from http://jamesonquave.com/blog/making-a-post-request-in-swift/
+    func postToApi(api:String){
         
-        let request = NSMutableURLRequest(url: NSURL(string: "http://wavy.azurewebsites.net/speech/text") as! URL)
+        let request = NSMutableURLRequest(url: NSURL(string: api) as! URL)
         let session = URLSession.shared
         request.httpMethod = "POST"
         
@@ -125,14 +129,30 @@ class InterfaceController: WKInterfaceController {
         let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error -> Void in
             
             NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(data) \n @resp: \(response) \n @err: \(error)   \n\n\n==========================================\n");
-            if let realData =  data {
-                let strData = NSString(data: realData, encoding: String.Encoding.utf8.rawValue)
-                NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(strData!)   \n\n\n==========================================\n");
-                self.resultLabel.setText(strData as String?);
+            if let realData = data {
+                self.writeReturnedAudioToFile(audioData:  realData)
+                // FOR TEXT_ENDPOINT
+                // let strData = NSString(data: realData, encoding: String.Encoding.utf8.rawValue)
+                // NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: \(strData!)   \n\n\n==========================================\n");
+                // self.resultLabel.setText(strData as String?);
             }
         })
         
         task.resume()
+    }
+    
+    
+    func writeReturnedAudioToFile(audioData: Data){
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let filePath = documentsPath.appending("result.wav")
+        let fileURL = URL(fileURLWithPath: filePath)
+        
+        do {
+            try audioData.write(to: fileURL)
+            self.playAudioWithMediaPlayer(url: fileURL)
+        } catch {
+            NSLog("%@", "\n==========================================\n\n\n Claudius Logging:\n @file: \(#file)\n @func: \(#function)\n @line: \(#line)\n @data: Error writing data to file   \n\n\n==========================================\n");
+        }
     }
     
     override func willActivate() {
@@ -144,5 +164,5 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
 }
